d7b003473d34557eba6013320ebd2a17da02e631
Andrew <githubmcandre.gumdrop164@passfwd.com>
2025-05-04 21:36:09 -0500

    up

--- install.mk
+++ install.mk
@@ -10,9 +10,9 @@ all: crates rustup-components
 crates:
 	cargo install --force \
 		cargo-audit \
-		crit@0.0.7 \
+		crit@0.0.8 \
 		cross@0.2.5 \
-		unmake@0.0.16
+		unmake@0.0.18
 
 rustup-components:
 	rustup component add \
