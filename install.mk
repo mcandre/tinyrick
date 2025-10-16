a44f64d11b186965ba5cd1a72f3e9cc2d6208f08
Andrew <githubmcandre.gumdrop164@passfwd.com>
2025-10-16 13:53:09 -0500

    update cross

--- install.mk
+++ install.mk
@@ -13,8 +13,12 @@ crates:
 		cargo-cache \
 		cargo-edit \
 		crit@0.0.10 \
-		cross@0.2.5 \
 		unmake@0.0.21
+
+	cargo install --force \
+		cross \
+			--git https://github.com/cross-rs/cross \
+			--rev 4e64366af6095c84fa4f54a0fa5a2ba7d9a271aa
 
 rustup-components:
 	rustup component add \
